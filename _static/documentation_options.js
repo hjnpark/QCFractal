b8579aaca9e3bf74471917ae3d02ec4fed1b9f67
hjnpark <54917187+hjnpark@users.noreply.github.com>
2023-10-13 19:10:21 +0000

    Deploying to gh-pages from @ hjnpark/QCFractal@8289de411fb8ff8652234760236d328012cf6f42 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.15.7.post1258+g23c41033',
+    VERSION: '0.15.7.post1266+g8289de41',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
