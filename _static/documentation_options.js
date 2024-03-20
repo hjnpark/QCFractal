77c93ba57dac0c03853771391e9196d0fe7a56c7
bennybp <ben@bennyp.org>
2024-03-20 14:01:56 +0000

    Deploying to gh-pages from @ hjnpark/QCFractal@5aad307c774879f689171b93d6a4fc8a2d498a38 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.15.7.post1390+g703bf4e4',
+    VERSION: '0.15.7.post1391+g5aad307c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
