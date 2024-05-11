4f528329cc90cfa52d3872e711f088937b8ee441
hjnpark <54917187+hjnpark@users.noreply.github.com>
2024-05-11 01:54:35 +0000

    Deploying to gh-pages from @ hjnpark/QCFractal@862383bdb4f28cb08c790e5ede1ab2ac9d7834a4 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.15.7.post1476+g614bb95b',
+    VERSION: '0.15.7.post1476+g862383bd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
